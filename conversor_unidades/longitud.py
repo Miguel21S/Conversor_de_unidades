5ab35aaffd7140dbbd95d06263871bb8cb431df1
Miguel21S <leaismiguelss@gmail.com>
2025-03-28 13:25:44 +0100

    Feat: funções kilometros_a_millas e  millas_a_metros na classe Longitude

--- conversor_unidades/longitud.py
+++ conversor_unidades/longitud.py
@@ -1,4 +1,12 @@
 class Longitud:
     @staticmethod
     def metro_a_kilometro(metro):
-        return metro / 1000
+        return f"Conversão de Metros a Kilometros: {metro / 1000}"
+    
+    @staticmethod
+    def kilometros_a_millas(kilometros):
+        return f"Conversão Kilometros a Millas: {kilometros * 0.621371}"
+
+    @staticmethod
+    def millas_a_metros(millas):
+        return (f"Conversão de Millas a Metros: {millas * 1609.34}")
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from conversor_unidades import Longitud, Temperatura
 print(Longitud.metro_a_kilometro(1500))
+print(Longitud.kilometros_a_millas(1500))
+print(Longitud.millas_a_metros(1500))
+
 print(Temperatura.celsius_a_fahrenheit(25))
